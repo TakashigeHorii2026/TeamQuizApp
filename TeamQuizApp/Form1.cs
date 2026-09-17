fa0815aa4aa885ffefa114b1d62df6654f077365
TakashigeHorii2026 <uni202607_10@telework.to>
2026-09-17 10:35:24 +0900

    Update Form1.cs

--- TeamQuizApp/Form1.cs
+++ TeamQuizApp/Form1.cs
@@ -56,7 +56,7 @@ namespace TeamQuizApp
 
         private void button3_Click(object sender, EventArgs e)
         {
-           // answerButton_Click(sender, e);
+            answerButton_Click(sender, e);
         }
 
         private void button4_Click(object sender, EventArgs e)
